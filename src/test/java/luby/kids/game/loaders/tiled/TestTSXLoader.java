678c59e86b69fa9fb6a974c64a3adb5e2d31ad8a
clubycoder <clubycoder@gmail.com>
2019-04-15 23:58:52 -0600

    Finished up tileset1 test

--- src/test/java/luby/kids/game/loaders/tiled/TestTSXLoader.java
+++ src/test/java/luby/kids/game/loaders/tiled/TestTSXLoader.java
@@ -29,28 +29,39 @@ public class TestTSXLoader {
                 set2D();
                 cam.setLocation(new Vector3f(0, 0, 0.5f));
 
-                Node tile1 = loadTile("tile-thin-grass.png");
-                logger.log(Level.INFO, "A Num Triangles: " + tile1.getTriangleCount());
-                tile1.move(0, settings.getHeight() / 2 - 64, 0);
-                guiNode.attachChild(tile1);
-
                 TSXTileSet tileSet = (TSXTileSet)assetManager.loadAsset("tileset1.tsx");
                 assertEquals(4, tileSet.getTileSet().getTiles().size());
                 tileSet.getTileSet().getTiles().keySet().forEach(tileId -> {
                     Node node = new Node(tileId + "-tile");
                     Geometry geometry = tileSet.getGeometry(tileId).clone(true);
-                    FloatBuffer uvs = (FloatBuffer)geometry.getMesh().getBuffer(VertexBuffer.Type.TexCoord).getData();
+                    FloatBuffer uvsBuffer = (FloatBuffer)geometry.getMesh().getBuffer(VertexBuffer.Type.TexCoord).getData();
+
                     System.out.print("GEO[" + tileId + "] "
                             + "name = " + geometry.getName() + ", "
                             + "material = " + geometry.getMaterial().getName() + ", "
                             + "UVs = "
                     );
                     for (int i = 0; i < 8; i += 2) {
-                        System.out.print("(" + uvs.get(i) + ", " + uvs.get(i + 1) + "), ");
+                        System.out.print("(" + uvsBuffer.get(i) + ", " + uvsBuffer.get(i + 1) + "), ");
                     }
                     System.out.println();
+
+                    assertEquals("tileset1-" + tileId + "-tile", geometry.getName());
+                    assertEquals("tileset1.png-tilemap", geometry.getMaterial().getName());
+                    float[] uvs = new float[8];
+                    for (int i = 0; i < 8; i += 1) {
+                        uvs[i] = uvsBuffer.get(i);
+                    }
+                    float[] uvsExpected = (
+                            tileId == 0 ? new float[] { 0.0f, 0.5f, 0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 1.0f } : (
+                            tileId == 1 ? new float[] { 0.5f, 0.5f, 1.0f, 0.5f, 1.0f, 1.0f, 0.5f, 1.0f } : (
+                            tileId == 2 ? new float[] { 0.0f, 0.0f, 0.5f, 0.0f, 0.5f, 0.5f, 0.0f, 0.5f } :
+                                          new float[] { 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.5f, 0.5f, 0.5f }
+                    )));
+                    assertArrayEquals(uvsExpected, uvs, 0.0f);
+
                     node.attachChild(geometry);
-                    node.move(tileId * 64, settings.getHeight() / 2, 0);
+                    node.move(tileId * 64, (float)settings.getHeight() / 2.0f, 0.0f);
                     guiNode.attachChild(node);
                 });
             }
